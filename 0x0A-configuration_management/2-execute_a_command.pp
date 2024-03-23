30453ca409821cecfe8e656c028ed5af1a1b5e02
ercudu <ilyass.bentalha2@gmail.com>
2024-03-23 02:49:16 +0000

    requirements

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # use command exec
-exec {'pkill':
-  command   => 'pkill -f killmenow',
-  path      => '/bin:/usr/bin',
+exec { 'pkill':
+  command => 'pkill -f killmenow',
+  path    => '/bin:/usr/bin',
 }
