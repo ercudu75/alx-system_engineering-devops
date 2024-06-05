90abcad9f7ebf2d539861c82e64ee1f9d4a404a0
ilyass <ilyass.bentalha2@gmail.com>
2024-06-05 01:25:39 +0100

    update task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,31 +1,16 @@
 #!/usr/bin/python3
-"""script for parsing web data from an api
-"""
-import json
+"""return subscribers"""
 import requests
-import sys
 
 
 def number_of_subscribers(subreddit):
-    """api call to reddit to get the number of subscribers
-    """
-    base_url = 'https://www.reddit.com/r/'
-    headers = {
-        'User-Agent':
-        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
-        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
-    }
-    # grab info about all users
-    url = base_url + '{}/about.json'.format(subreddit)
-    response = requests.get(url, headers=headers)
-    resp = json.loads(response.text)
-
-    try:
-        # grab the info about the users' tasks
-        data = resp.get('data')
-        subscribers = data.get('subscribers')
-    except:
+    """return subscribers"""
+    if not subreddit or not isinstance(subreddit, str):
         return 0
-    if subscribers is None:
-        return 0
-    return int(subscribers)
+    r = requests.get(
+        'https://www.reddit.com/r/{}/about.json'.format(subreddit),
+        headers={'User-Agent': "omar"})
+    if r.status_code == 200:
+        data = r.json()["data"]
+        return data["subscribers"]
+    return 0
